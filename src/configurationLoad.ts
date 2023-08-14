b8da2eaacffe5e8698502c2739f014b87aa279c6
renatus-cartesius <cartesius.absolute@gmail.com>
2023-08-15 02:03:00 +0300

    chore(docker): reworked Dockerfile to multistage usage

--- src/configurationLoad.ts
+++ src/configurationLoad.ts
@@ -6,6 +6,6 @@ const YAML_CONFIG_FILENAME = 'config.yml'
 
 export default () => {
   return yaml.load(
-    readFileSync(join(__dirname, "/config",  YAML_CONFIG_FILENAME), 'utf8'),
+    readFileSync('/etc/hbserver/config.yml', 'utf8'),
   ) as Record<string, any>;
 }
