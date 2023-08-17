4286fe62bddd5445480f1b12ba579705a86a82e6
renatus-cartesius <cartesius.absolute@gmail.com>
2023-08-17 05:55:41 +0300

    chore: added listAll operations-controller method

--- src/api/operations/operations.controller.ts
+++ src/api/operations/operations.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HbdriverService } from 'src/hbdriver/hbdriver.service';
 
 @Controller('api/operations')
-export class OperationsController {}
+export class OperationsController {
+  constructor(private readonly hbdriverService: HbdriverService) {}
+
+  @Get('all')
+  listAll(){
+    return this.hbdriverService.operations;
+  }
+}
